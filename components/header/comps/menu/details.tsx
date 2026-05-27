21059a4f9b446514899993d9648a7b19eaaf3f83
adrianl01 <ga.leiva.1601@gmail.com>
2026-05-27 19:43:30 -0300

    commented files

--- components/header/comps/menu/details.tsx
+++ components/header/comps/menu/details.tsx
@@ -1,49 +1,49 @@
-import { useState, useRef } from "react";
-import { set } from "react-hook-form";
+// import { useState, useRef } from "react";
+// import { set } from "react-hook-form";
 
-export function OptionsComp() {
-  const [open, setOpen] = useState(false);
-  const btnRef = useRef<HTMLButtonElement>(null);
+// export function OptionsComp() {
+//   const [open, setOpen] = useState(false);
+//   const btnRef = useRef<HTMLButtonElement>(null);
 
-  return (
-    <div
-      className="flex flex-col relative"
-      onMouseEnter={() => setOpen(true)}
-    >
-      <button
-        ref={btnRef}
-        className="flex items-center text-white border-solid border-white border-[2px] rounded-lg p-2 bg-black hover:bg-gray-800 transition-all duration-300 w-full"
-      >
-        <span
-          className={`transition-transform duration-300 ${
-            open ? "rotate-180" : "rotate-0"
-          }`}
-        >
-          ▲
-        </span>
-      </button>
-      {open && (
-        <div
-          className="absolute top-full mb-2 right-0 flex flex-col w-56 border-transparent bg-transparent z-50"
-          onMouseLeave={() => setOpen(false)}
-        >
-          <div className="flex flex-col  p-2 gap-2 mt-2 border-solid border-white border-[2px] rounded-lg bg-black">
-            <a
-              href="/profile/orders"
-              className="hover:bg-pink-400 text-white hover:text-black rounded px-2 py-1 transition"
-            >
-              Your Orders
-            </a>
-            <a
-              href="/profile"
-              className="hover:bg-pink-400 text-white hover:text-black rounded px-2 py-1 transition"
-              onMouseEnter={() => setOpen(true)}
-            >
-              Your Profile
-            </a>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       className="flex flex-col relative"
+//       onMouseEnter={() => setOpen(true)}
+//     >
+//       <button
+//         ref={btnRef}
+//         className="flex items-center text-white border-solid border-white border-[2px] rounded-lg p-2 bg-black hover:bg-gray-800 transition-all duration-300 w-full"
+//       >
+//         <span
+//           className={`transition-transform duration-300 ${
+//             open ? "rotate-180" : "rotate-0"
+//           }`}
+//         >
+//           ▲
+//         </span>
+//       </button>
+//       {open && (
+//         <div
+//           className="absolute top-full mb-2 right-0 flex flex-col w-56 border-transparent bg-transparent z-50"
+//           onMouseLeave={() => setOpen(false)}
+//         >
+//           <div className="flex flex-col  p-2 gap-2 mt-2 border-solid border-white border-[2px] rounded-lg bg-black">
+//             <a
+//               href="/profile/orders"
+//               className="hover:bg-pink-400 text-white hover:text-black rounded px-2 py-1 transition"
+//             >
+//               Your Orders
+//             </a>
+//             <a
+//               href="/profile"
+//               className="hover:bg-pink-400 text-white hover:text-black rounded px-2 py-1 transition"
+//               onMouseEnter={() => setOpen(true)}
+//             >
+//               Your Profile
+//             </a>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
--- components/header/comps/menu/index.tsx
+++ components/header/comps/menu/index.tsx
@@ -1,61 +1,61 @@
-import { CloseMenuButton, MenuButton } from "@/components/ui/buttons";
-import Link from "next/link";
-import { getEmail } from "@/lib/storage";
-import { logout, retrieveToken } from "@/lib";
-import { useRouter } from "next/navigation";
+// import { CloseMenuButton, MenuButton } from "@/components/ui/buttons";
+// import Link from "next/link";
+// import { getEmail } from "@/lib/storage";
+// import { logout, retrieveToken } from "@/lib/api";
+// import { useRouter } from "next/navigation";
 
-export function Menu(props: any) {
-  const router = useRouter();
-  const closeMenuHandler = (e: any) => {
-    e.preventDefault();
-    props.closeMenu(false);
-  };
-  const token = retrieveToken();
-  const email = getEmail();
+// export function Menu(props: any) {
+//   const router = useRouter();
+//   const closeMenuHandler = (e: any) => {
+//     e.preventDefault();
+//     props.closeMenu(false);
+//   };
+//   const token = retrieveToken();
+//   const email = getEmail();
 
-  function ShowEmail(props: any) {
-    return <div className="text-[23px]">{props.emailProp}</div>;
-  }
+//   function ShowEmail(props: any) {
+//     return <div className="text-[23px]">{props.emailProp}</div>;
+//   }
 
-  return (
-    <div className="bg-black flex flex-col justify-between text-[42px] text-center text-white p-[15px] h-[100%] w-[100%] m-0">
-      <div className="flex justify-end">
-        <CloseMenuButton handler={closeMenuHandler} />
-      </div>
-      <div className="MenuButtonBox">
-        {token ? (
-          <></>
-        ) : (
-          <Link href={"/login"}>
-            <MenuButton>Log In</MenuButton>
-          </Link>
-        )}
-        {token ? (
-          <Link href={"/profile"}>
-            <MenuButton>My Profile</MenuButton>
-          </Link>
-        ) : (
-          <></>
-        )}
-        <Link href={"/search"}>
-          <MenuButton>Search</MenuButton>
-        </Link>
-      </div>
-      <ShowEmail emailProp={email} />
+//   return (
+//     <div className="bg-black flex flex-col justify-between text-[42px] text-center text-white p-[15px] h-[100%] w-[100%] m-0">
+//       <div className="flex justify-end">
+//         <CloseMenuButton handler={closeMenuHandler} />
+//       </div>
+//       <div className="MenuButtonBox">
+//         {token ? (
+//           <></>
+//         ) : (
+//           <Link href={"/login"}>
+//             <MenuButton>Log In</MenuButton>
+//           </Link>
+//         )}
+//         {token ? (
+//           <Link href={"/profile"}>
+//             <MenuButton>My Profile</MenuButton>
+//           </Link>
+//         ) : (
+//           <></>
+//         )}
+//         <Link href={"/search"}>
+//           <MenuButton>Search</MenuButton>
+//         </Link>
+//       </div>
+//       <ShowEmail emailProp={email} />
 
-      {token ? (
-        <button
-          className="bg-black border-none text-[20px] font-normal text-center text-red-600"
-          onClick={() => {
-            logout();
-            router.push("/");
-          }}
-        >
-          Log Out
-        </button>
-      ) : (
-        <div></div>
-      )}
-    </div>
-  );
-}
+//       {token ? (
+//         <button
+//           className="bg-black border-none text-[20px] font-normal text-center text-red-600"
+//           onClick={() => {
+//             logout();
+//             router.push("/");
+//           }}
+//         >
+//           Log Out
+//         </button>
+//       ) : (
+//         <div></div>
+//       )}
+//     </div>
+//   );
+// }
